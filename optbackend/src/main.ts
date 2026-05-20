3b77ac4b84da8c5d9073e3f064574a204f944ee7
Makram <m.jemaa.2025@alumnos.urjc.es>
2026-05-20 14:22:27 +0200

    Bind server to all network interfaces

--- optbackend/src/main.ts
+++ optbackend/src/main.ts
@@ -12,7 +12,7 @@ async function bootstrap() {
   );
   app.useGlobalFilters(new HttpExceptionFilter());
   const port = process.env.PORT ?? 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`Backend running on http://localhost:${port}`);
 }
 bootstrap();
