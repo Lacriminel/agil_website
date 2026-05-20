c182d7c20ca17c2fa79fe0dad5b27d8cfd78002e
Makram <m.jemaa.2025@alumnos.urjc.es>
2026-05-20 15:23:44 +0200

    Update API base URL to production endpoint

--- OPTfront/src/app/core/services/api.service.ts
+++ OPTfront/src/app/core/services/api.service.ts
@@ -1,7 +1,7 @@
 ﻿import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
-const BASE = 'http://localhost:3000';
+const BASE = 'https://agil-backend-leq8.onrender.com';
 
 @Injectable({ providedIn: 'root' })
 export class ApiService {
